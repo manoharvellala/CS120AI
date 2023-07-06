0223c0d7b377fface83ffd6179649c3d83e9a435
Manohar <manohar@Manohars-MacBook-Pro.local>
2023-07-06 16:21:16 +0530

    changes

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -11,6 +11,36 @@ export class AuthenticationComponent {
   constructor(private router: Router, public auth: AuthService) {}
 
   login(): void {
-    this.auth.loginWithRedirect();
+    this.auth
+      .loginWithRedirect({
+        appState: {
+          target: window.location.origin + '/layout',
+        },
+      })
+      .subscribe(
+        () => {
+          // After successful login, handle the redirect callback
+          this.auth.handleRedirectCallback().subscribe(
+            () => {
+              // Redirect to the target URL
+              const targetUrl = window.location.origin + '/layout';
+              if (targetUrl) {
+                window.location.href = targetUrl;
+              } else {
+                // If no target URL is specified, redirect to a default page
+                window.location.href = '/default-page';
+              }
+            },
+            (error) => {
+              // Handle error while processing redirect callback
+              console.error('Error handling redirect callback:', error);
+            }
+          );
+        },
+        (error) => {
+          // Handle error during login
+          console.error('Error during login:', error);
+        }
+      );
   }
 }
