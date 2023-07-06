800747469b91575b83603215d005c3a7da805e0b
Manohar <manohar@Manohars-MacBook-Pro.local>
2023-07-06 16:39:29 +0530

    changes

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, UrlSerializer } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -8,33 +8,22 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./authentication.component.css'],
 })
 export class AuthenticationComponent {
-  constructor(private router: Router, public auth: AuthService) {}
+  constructor(
+    private router: Router,
+    private urlSerializer: UrlSerializer,
+    public auth: AuthService
+  ) {}
 
   login(): void {
-    this.auth.loginWithRedirect().subscribe(
-      () => {
-        // After successful login, handle the redirect callback
-        this.auth.handleRedirectCallback().subscribe(
-          () => {
-            // Redirect to the target URL
-            const targetUrl = window.location.origin + '/layout';
-            if (targetUrl) {
-              window.location.href = targetUrl;
-            } else {
-              // If no target URL is specified, redirect to a default page
-              window.location.href = '/default-page';
-            }
-          },
-          (error) => {
-            // Handle error while processing redirect callback
-            console.error('Error handling redirect callback:', error);
-          }
-        );
-      },
-      (error) => {
-        // Handle error during login
-        console.error('Error during login:', error);
-      }
+    const targetUrl = this.urlSerializer.serialize(
+      this.router.createUrlTree(['/layout'])
     );
+    const cleanedTargetUrl = targetUrl.split('?')[0]; // Remove query parameters
+
+    this.auth.loginWithRedirect({
+      appState: {
+        target: cleanedTargetUrl,
+      },
+    });
   }
 }
